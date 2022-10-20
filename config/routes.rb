95ba80c9f025f3362c0602e276c3242aeec0d729
Igor Portela <igor@mail.com>
2022-10-20 16:01:07 -0300

    upgrading to rails 6 version and fix api version to v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-    get 'books' => 'books#index'
-    get 'books/:id' => 'books#show'
-    post 'books' => 'books#create'
-    delete 'books/:id' => 'books#destroy'
-    patch 'books/:id' => 'books#update'
+    namespace :v1 do
+      get 'books' => 'books#index'
+      get 'books/:id' => 'books#show'
+      post 'books' => 'books#create'
+      delete 'books/:id' => 'books#destroy'
+      patch 'books/:id' => 'books#update'
+    end
   end
 end
