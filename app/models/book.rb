99a193b646c3f5397248bb06c80840106e84e1a6
Igor Portela <igor@mail.com>
2022-10-20 21:35:53 -0300

    book table and model updated

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :stock, numericality: { only_integer: true }
+
+  before_create -> (book) do
+    book.slug = book.title.parameterize
+  end
 end
