3be4821fc549e1b35b243b29927f214608163555
Igor Portela | user:igoracportela <igorcostaportela@gmail.com>
2022-10-25 18:59:21 -0300

    feat/seed and new fields to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,11 @@
 class Book < ApplicationRecord
   has_many :reviews
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, :stock, presence: true
+  validates :title, uniqueness: true
   validates :stock, numericality: { only_integer: true }
 
-  before_create -> (book) do
+  before_create lambda { |book|
     book.slug = book.title.parameterize
-  end
+  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,67 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create!([
+               {
+                 title: 'The well-Grounded Rubyist',
+                 cover_image_url: 'https://m.media-amazon.com/images/I/51qtp4NzjRL._SX397_BO1,204,203,200_.jpg',
+                 stock: 10
+               },
+               {
+                 title: 'Head First Ruby: A Brain-Friendly Guide',
+                 cover_image_url: 'https://m.media-amazon.com/images/I/51PDAiY3y1L._SX430_BO1,204,203,200_.jpg',
+                 stock: 10
+               },
+               {
+                 title: 'Ruby Programming for Beginners',
+                 cover_image_url: 'https://m.media-amazon.com/images/I/41mkoeX-7fL._SY344_BO1,204,203,200_QL70_ML2_.jpg',
+                 stock: 10
+               },
+               {
+                 title: 'The Ruby Programming Language: Everything You Need to Know',
+                 cover_image_url: 'https://m.media-amazon.com/images/I/51Uz161-sgL._SX258_BO1,204,203,200_QL70_ML2_.jpg',
+                 stock: 10
+               },
+               {
+                 title: 'Ruby Cookbook: Recipes for Object-Oriented Scripting',
+                 cover_image_url: 'https://m.media-amazon.com/images/I/51PUsTrP7-L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+                 stock: 10
+               },
+               {
+                 title: 'Polished Ruby Programming',
+                 cover_image_url: 'https://m.media-amazon.com/images/I/4112DUgEekS._SX404_BO1,204,203,200_.jpg',
+                 stock: 10
+               },
+               {
+                 title: 'The Ruby Way: Solutions and Techniques in Ruby Programming',
+                 cover_image_url: 'https://m.media-amazon.com/images/I/4112DUgEekS._SX404_BO1,204,203,200_.jpg',
+                 stock: 10
+               },
+               {
+                 title: 'Beginning Ruby 3: From Beginner to Pro',
+                 cover_image_url: 'https://m.media-amazon.com/images/I/41XigrzSHnL._SX348_BO1,204,203,200_.jpg',
+                 stock: 10
+               },
+               {
+                 title: 'Programming Ruby: The Pragmatic Programmers Guide',
+                 cover_image_url: 'https://m.media-amazon.com/images/I/41gtODXuRlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+                 stock: 10
+               },
+               {
+                 title: 'Ruby Performance Optimization: Why Ruby is Slow, and How to Fix It',
+                 cover_image_url: 'https://m.media-amazon.com/images/I/51nNW87Uv3L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+                 stock: 10
+               }
+             ])
+
+Review.create([
+                {
+                  title: 'Great Book',
+                  description: 'I really love it',
+                  score: 5,
+                  book: Book.find_by_title('Programming Ruby: The Pragmatic Programmers Guide')
+                },
+                {
+                  title: 'Basic Book',
+                  description: "I'm a beginner, even so I found it very basic",
+                  score: 3,
+                  book: Book.find_by_title('Ruby Programming for Beginners')
+                }
+              ])
